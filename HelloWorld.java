04bba647f692bbc04eb57b8b13575bb8f6223106
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-09-29 17:55:27 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld
     public static void main(String[] Args)
     {   
         System.out.println("Hello World");
+
+    //This is a single line comment
+
+    /* This is 
+     * a multi line comment...
+     * .....
+     */
+
+        // The comments are not seen when the code is executed.
     }
 }
