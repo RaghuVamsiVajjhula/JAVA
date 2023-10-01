cae44b18a8504c12ec7ecd1ec3bd0b9ac01db7d3
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-10-01 17:26:55 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,30 @@
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int x; //declaration
+        x=100;//assignment
+
+        //or
+
+        int y=200;//initialisation
+
+         char letter='$';
+        String name="Hello";
+        boolean z=true;
+        double number= 3.14;
+
+
+        System.out.println(x);
+        System.out.println(y);
+        
+        System.out.println("The variable value of x is:"+x);
+        System.out.println("The value in the string name is:"+name);
+        System.out.println("THe value of double is:"+number);
+        System.out.println("Value of char is:"+letter);
+        System.out.println("Value of boolena is:"+z);
+
+       
+    }
+}
