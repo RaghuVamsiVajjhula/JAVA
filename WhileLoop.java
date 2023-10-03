774aca90db4a86c0bf6b46836fd71f53b925ce9e
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-10-03 20:52:19 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a prorgam to print while loop:");
+        int i=0;
+        while(i<10)
+        {
+            System.out.println("The value is: "+i);
+            i++;
+        }
+    }
+    
+}
