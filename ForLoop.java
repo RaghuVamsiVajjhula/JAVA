ba7e1b5783906f4974b4f5cedfefeb076b0e4e9b
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-10-03 20:52:35 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+
+public class ForLoop
+{
+    public static void main(String[] args)
+    {
+        System.out.println("The for loop:");
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println("The value will be:"+i);
+            i++;
+        }
+    }
+}
