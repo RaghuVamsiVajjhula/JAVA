0951796f7a6ca985c397c71abf10e6e2684f9135
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-10-06 19:17:19 +0530

    Add files via upload

---
+++ NestedLoops.java
@@ -0,0 +1,23 @@
+
+public class NestedLoops 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is for Nested Loops.");
+        //Nested loopes are a loop inside another loop.
+
+        int i,j;
+        // int  num[];
+        
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                System.out.println("Value of i: "+i);
+                System.out.println("Value of j: "+j);
+            }
+        }
+       
+    }
+    
+}
